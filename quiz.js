62bd0aea09f28807fcc8dbcc8abd24cd1b51785e
Emmy Schippnick <eschippnick@gmail.com>
2019-12-15 18:03:13 -0800

    functioning quiz and timer

--- quiz.js
+++ quiz.js
@@ -7,82 +7,88 @@ var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var counter = document.getElementById("counter");
 var progress = document.getElementById("progress");
-
-
 var scoreDiv = document.getElementById("scoreContainer");
 
-// create our questions     **********  Supposed to be a separate doc but how?
+var belowQuiz = document.getElementById("bottomOfPage");
+
+var submitHighScore = document.getElementById("sub");
+
+var highScore = document.getElementById("submitMe");
+
+// create our questions
 var questions = [{
-        question: "What does HTML stand for?",
-        imgSrc: "img/html.png",
-        choiceA: "Correct",
-        choiceB: "Wrong",
-        choiceC: "Wrong",
+        question: "The Earth revolves around?",
+        choiceA: "The Sun",
+        choiceB: "Itself",
+        choiceC: "The Moon",
         correct: "A"
     }, {
-        question: "What does CSS stand for?",
-        imgSrc: "img/css.png",
-        choiceA: "Wrong",
-        choiceB: "Correct",
-        choiceC: "Wrong",
+        question: "What is the sun?",
+        choiceA: "The sun is a planet",
+        choiceB: "The sun is a star",
+        choiceC: "The sun is a satellite",
         correct: "B"
     }, {
-        question: "What does JS stand for?",
-        imgSrc: "img/js.png",
+        question: "What is the closest planet to the sun?",
+        choiceA: "Venus",
+        choiceB: "Mars",
+        choiceC: "Mercury",
+        correct: "C"
+    },
+    {
+        question: "What color is the sky",
+        choiceA: "Pink",
+        choiceB: "Green",
+        choiceC: "Blue",
+        correct: "C"
+    }, {
+        question: "What year did man land on the Moon?",
+        choiceA: "1989",
+        choiceB: "1979",
+        choiceC: "1969",
+        correct: "C"
+    }, {
+        question: "What is the circumference of the Earth?",
+
+        choiceA: "27,802 miles",
+        choiceB: "24,901 miles",
+        choiceC: "22,107 miles",
+        correct: "B"
+    }, {
+        question: "The Earth revolves around?",
+        choiceA: "The Sun",
+        choiceB: "Itself",
+        choiceC: "The Moon",
+        correct: "A"
+    }, {
+        question: "Why not",
         choiceA: "Wrong",
         choiceB: "Wrong",
         choiceC: "Correct",
         correct: "C"
-    },
-    {
-        question: "What does JS stand for?",
-        imgSrc: "img/js.png",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        correct: "C"
-    },
-    {
-        question: "What does JS stand for?",
-        imgSrc: "img/js.png",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        correct: "C"
-    },
-    {
-        question: "What does emmy stand for?",
-        imgSrc: "img/js.png",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        correct: "C"
-    },
-    {
-        question: "What does jkdsfHKASDHF stand for?",
-        imgSrc: "img/js.png",
-        choiceA: "Wrong",
-        choiceB: "Wrong",
-        choiceC: "Correct",
-        correct: "C"
-    },
-    {
-        question: "What does JS stand for?",
-        imgSrc: "img/js.png",
+    }, {
+        question: "The Earth revolves around?",
+        choiceA: "The Sun",
+        choiceB: "Itself",
+        choiceC: "The Moon",
+        correct: "A"
+    }, {
+        question: "Helllloooo?",
         choiceA: "Wrong",
         choiceB: "Wrong",
         choiceC: "Correct",
         correct: "C"
     }
-
 ];
+
 
 // create some variables
 
 var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
-var count = 0;
-
+var count = 150;
+var totalTime = 150;
+var TIMER;
 var score = 0;
 
 // render a question
@@ -102,77 +108,51 @@ function startQuiz() {
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
-    TIMER = setInterval(renderCounter, 1000);
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
-// function renderProgress() {
-//     for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-//         progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-//     }
-// }
-
-
-
-
-
-
-// Needs to be triggered when the start button is clicked
-
-var TIMER = setInterval(timer, 1000); // renamed?
-
-var counter = 0;
-var min = 4;
-var sec = 60;
-
-function timer() {
-
-    sec = --sec;
-
-    if (sec === 0) {
-        min = --min;
-        sec = 59;
-        counter = ++counter;
-    }
-
-    if (counter === 5) {
-        sec = 0;
-        min = 0;
-        clearInterval(TIMER);
-        functionSubmit(); //    How to finish the quiz, last 3 pop ups??????
-        document.write("Submitted");
-    }
-
-    document.getElementById("timer").innerHTML = "<h1>" + min + " : " + sec + "<h1>";
-
-}
-
-
+// render progress
 function renderProgress() {
     for (var qIndex = 0; qIndex <= lastQuestion; qIndex++) {
         progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
     }
 }
 
+// counter render
+
 
 function renderCounter() {
-    // if (count <= 0) {
-    //     counter.innerHTML = count;
-    //     // timeGauge.style.width = count * gaugeUnit + "px";
-    //     count++
-    // } else {
-    //     count = 0;
-    //     // change progress color to red
-    //     answerIsWrong();
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        renderQuestion();
+    if (count <= totalTime) {
+        counter.innerHTML = count;
+
+        count--
     } else {
-        // end the quiz and show the score
+        count = 0;
         clearInterval(TIMER);
         scoreRender();
 
+        answerIsWrong();
+        if (runningQuestion < lastQuestion) {
+            runningQuestion++;
+            renderQuestion();
+        } else {
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
     }
 }
+
+// function endQuiz() {
+//     if (count = 0) {
+//         clearInterval(TIMER);
+//         scoreRender();
+//     }
+// }
+
+// checkAnwer
 
 
 
@@ -181,20 +161,21 @@ function checkAnswer(answer) {
         // answer is correct
         score++;
         // change progress color to green
-        // answerIsCorrect();
+        answerIsCorrect();
     } else {
+        count = count - 10;
         // answer is wrong
         // change progress color to red
-        // answerIsWrong();
+        answerIsWrong();
+
     }
-    count = 0;
     if (runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
     } else {
         // end the quiz and show the score
         clearInterval(TIMER);
-        // scoreRender();
+        scoreRender();
     }
 }
 
@@ -206,48 +187,60 @@ function checkAnswer(answer) {
 
 
 
-// // answer is correct
-// function answerIsCorrect() {
-//     document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-// }
 
-// // answer is Wrong
-// function answerIsWrong() {
-//     document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-// }
+// answer is correct
+function answerIsCorrect() {
+    document.getElementById(runningQuestion).style.backgroundColor = "#33FFF7";
+}
+
+// answer is Wrong
+function answerIsWrong() {
+    document.getElementById(runningQuestion).style.backgroundColor = "#33FFF7";
+}
+
+
+
+
+function CheckTime() {
+
+    document.getElementById("timer").innerHTML = "Time remaining: " + count + " seconds";
+
+    if (count <= 0) {
+        scoreRender();
+    } else {
+        // count = count - 1;
+        timer = setTimeout(CheckTime, 1000);
+    }
+}
+timer = setTimeout(CheckTime, 1000);
+
+
+
+
+
+
 
 // score render
 function scoreRender() {
     scoreDiv.style.display = "block";
+
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score / questions.length);
+
+
+
+    scoreDiv.innerHTML = "<p>" + "Your final score is " + scorePerCent + "%</p>";
+    // + "<br>" + "<p>" + "Enter your name below" + "</p>";
+    // scoreDiv.innerHTML += "Enter your name: " + "<input type='text' name='usrname'>" +
+    //     "<input type='submit' value='Submit'>";
+
+    belowQuiz.innerHTML = "Enter your name: " + "<input type='text' name='usrname'>" +
+        "<input id='sub' type='submit' value='Submit'>";
+
+    // function submitHighScore {
+    //     document.getElementById("sub")
+    // }
+
+
+
 }
-
-// checkAnwer
-
-// function checkAnswer(answer) {
-//     if (answer == questions[runningQuestion].correct) {
-//         score++;
-//         answerIsCorrect();
-//     } else {
-//         answerIsWrong();
-//         count = 0;
-//         if (runningQuestion < lastQuestion) {
-//             runningQuestion++;
-//             renderQuestion();
-//         } else {
-//             // end the quiz and show the score
-//             clearInterval(TIMER);
-//             scoreRender();
-//         }
-
-
-
-
-// calculate the amount of question percent answered by the user
-var scorePerCent = Math.round(100 * score / questions.length);
-
-// // choose the image based on the scorePerCent
-// var img = (scorePerCent >= 70) ? " images/greatjob.png" :
-//     (scorePerCent >= 0) ? " images/youtried.png" :
-
-// scoreDiv.innerHTML = "<img src=" + img + ">";
-// scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
